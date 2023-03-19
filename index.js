c1f737787d51062e6d614323fa1779b132d7a8f0
amyskaletzky <amy.skaletzky@gmail.com>
2023-03-19 11:09:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors())
+
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello from server!' })
+})
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server listening on ${process.env.PORT || 3001}`);
+})
